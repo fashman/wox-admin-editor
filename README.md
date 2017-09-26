f6c0d563bbb7730dcb950b49802228c21936a906
FrendEr <frend.wong@gmail.com>
2017-09-26 16:14:09 +0800

    组件优化

--- README.md
+++ README.md
@@ -4,33 +4,16 @@
 
 | 属性 | 说明 | 类型 | 默认值 |
 | ---- | ---- | ---- | ---- |
-| callback | 回调函数，返回{ keyName：当前值 } | Function | |
+| callback | 回调函数，返回 `{ keyName: 当前值 }` | Function | |
 | keyName | 返回对象的属性名 | String | value |
 | readOnly | 是否只读 | Boolean | false |
 | value | 富文本框值 | String | |
-| toolbar | 功能栏 | Object | |
+| toolbar | 功能栏，如需自定义配置，请参考 `[默认配置](./src/index.jsx#L15)` | Object | |
 
-## import
+## Usage
 
 ```javascript
-$ npm install wox-admin-edit
-```
+import WoxEditor from 'wox-admin-editor';
+import 'wox-admin-editor/lib/index.css';
 
-## toolbar
-> 默认**toolbar**为：
-```javascript
-const defaultToolbar = {
-  textStyle: ['bold', 'italic', 'underline', 'strike'],
-  quote: ['blockquote'],
-  header: [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-  list: [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  indent: [{ 'indent': '-1'}, { 'indent': '+1' }],
-  size: [{ 'size': ['small', false, 'large', 'huge'] }],
-  color: [{ 'color': [] }, { 'background': [] }],
-  font: [{ 'font': [] }],
-  align: [{ 'align': [] }],
-  liv: [ 'link', 'image', 'video' ],
-  clean: ['clean']
-};
-```
-> 自定义 **toolbar**请按这种格式书写
+ReactDOM.render(<WoxEditor />, rootEle);
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
-/**
- * callback[Function]					//回调函数，返回{ keyName：当前值 }
- * keyName[String]						//返回对象的属性名
- * readOnly[Boolean]					//是否只读
- * value[String]							//富文本框值
- * 组件值的更改会触发回调函数，需要更新传入的value以达到更新组件的值
-**/
-
 import React, { Component } from 'react';
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css';
 
+const colors = [
+  '#000000', '#444444', '#666666', '#999999', '#cccccc', '#eeeeee', '#f3f3f3', '#ffffff',
+  '#ff0000', '#ff9900', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#9900ff', '#ff00ff',
+  '#f4cccc', '#fce5cd', '#fff2cc', '#d9ead3', '#d0e0e3', '#cfe2f3', '#d9d2e9', '#ead1dc',
+  '#ea9999', '#f9cb9c', '#ffe599', '#b6d7a8', '#a2c4c9', '#9fc5e8', '#b4a7d6', '#d5a6bd',
+  '#e06666', '#f6b26b', '#ffd966', '#93c47d', '#76a5af', '#6fa8dc', '#8e7cc3', '#c27ba0',
+  '#cc0000', '#e69138', '#f1c232', '#6aa84f', '#45818e', '#3d85c6', '#674ea7', '#a64d79',
+  '#990000', '#b45f06', '#bf9000', '#38761d', '#134f5c', '#0b5394', '#351c75', '#741b47',
+  '#660000', '#783f04', '#7f6000', '#274e13', '#0c343d', '#073763', '#20124d', '#4c1130'
+];
 const defaultToolbar = {
   textStyle: ['bold', 'italic', 'underline', 'strike'],
   quote: ['blockquote'],
@@ -17,23 +19,26 @@ const defaultToolbar = {
   list: [{ 'list': 'ordered'}, { 'list': 'bullet' }],
   indent: [{ 'indent': '-1'}, { 'indent': '+1' }],
   size: [{ 'size': ['small', false, 'large', 'huge'] }],
-  color: [{ 'color': [] }, { 'background': [] }],
+  color: [{ 'color': colors }, { 'background': colors }],
   font: [{ 'font': [] }],
   align: [{ 'align': [] }],
   liv: [ 'link', 'image', 'video' ],
   clean: ['clean']
 };
 
-export default class EditorCom extends Component {
+export default class WoxEditor extends Component {
+
   handleChange = (value) => {
     const key = this.props.keyName || 'value';
     this.props.callback( {[key]: value} );
   }
+
   render() {
     const toolbar = Object.assign({}, defaultToolbar, this.props.toolbar || {});
     const modules = { toolbar: [] };
+
     for (let i in toolbar ){
-      modules.toolbar.push( toolbar[i] );
+      modules.toolbar.push(toolbar[i]);
     }
     return (
       <ReactQuill
